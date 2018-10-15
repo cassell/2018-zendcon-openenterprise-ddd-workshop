b2916df056869f0ddeae2e1d621cfdbeb271b74a
Andrew Cassell <cassell@users.noreply.github.com>
2018-10-15 09:23:58 -0700

    solution for degree celsisus unit test

--- src/Domain/Measurements/Temperature/DegreesCelsius.php
+++ src/Domain/Measurements/Temperature/DegreesCelsius.php
@@ -8,9 +8,21 @@ class DegreesCelsius implements Temperature
     use TemperatureStringFormat;
 
     private const SYMBOL = '°C';
+    const FLOAT_PRECISION = 3;
+
+    private $value;
 
     public function __construct(float $value)
     {
+        $this->value = round($value, self::FLOAT_PRECISION);
+
+        if ($this->value < AbsoluteZero::IN_CELSIUS) {
+            throw new AbsoluteZeroException();
+        }
+
+        if ($this->value === -0.0) {
+            $this->value = 0.0;
+        }
 
     }
 
@@ -21,12 +33,12 @@ class DegreesCelsius implements Temperature
 
     public static function fromFahrenheit(DegreesFahrenheit $fahrenheit): self
     {
-        return new self(0);
+        return new self(($fahrenheit->getValue() - 32.0) * 5 / 9);
     }
 
     public function getValue(): float
     {
-        return 0;
+        return $this->value;
 
     }
 
